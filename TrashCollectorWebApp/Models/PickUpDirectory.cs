d7e0f274dee45880fbbe0df7e5fd1a9b39619814
buck-nicholas <buck.nicholas.c@gmail.com>
2018-07-27 09:04:56 -0500

    Added PickUpDirectory table

---
+++ TrashCollectorWebApp/Models/PickUpDirectory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrashCollectorWebApp.Models
+{
+    public class PickUpDirectory
+    {
+        [Key]
+        public int ID { get; set; }
+        public string DayOfWeek { get; set; }
+        public bool SpecialPickUp { get; set; }
+        public string SpecialDate { get; set; }
+        public bool PickUpCompleted { get; set; }
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
